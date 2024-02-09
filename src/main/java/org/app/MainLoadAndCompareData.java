71ffddedba1e751da2a47eedf5f3c6e307ce73e6
Martin Cé <72087021+Guestas@users.noreply.github.com>
2024-02-09 12:58:15 +0100

    updated main

--- src/main/java/org/app/MainLoadAndCompareData.java
+++ src/main/java/org/app/MainLoadAndCompareData.java
@@ -2,7 +2,7 @@ package org.app;
 
 import org.app.consoleThread.ConsoleApp;
 
-public class Main {
+public class MainLoadAndCompareData {
     public static void main(String[] args) throws InterruptedException {
         ConsoleApp app = new ConsoleApp();
         app.start();
